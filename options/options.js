61b2968802a1f10e9f9b76f55175355bb107cdc9
Mario S Garcia <mariogarciaph@gmail.com>
2023-05-10 01:24:58 +0800

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "dream-mobile-app-40586",
-  url: "https://dream-mobile-app-40586.botics.co",
-  api: "https://dream-mobile-app-40586.botics.co/api/v1"
-}
+  "name": "dream-mobile-app-40586",
+  "url": "https://dream-mobile-app-40586.botics.co",
+  "api": "https://dream-mobile-app-40586.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
